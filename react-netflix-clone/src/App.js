cd1c8dfb0b961ac3cb1e789a6e4bd0c3329e5e78
Jonathan Chen <jonathanchenzheng@hotmail.com>
2021-02-09 12:38:25 -0500

    Create Row component

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import React from 'react'
+import './App.css'
+import Row from './row'
+
 
 function App() {
   return (
     <div className="App">
-      Hey! Let's build a Netflix clone 🚀 
+      <h1>Hey! Let's build a Netflix clone 🚀</h1>
+      <Row title="Netflix Originals"/> 
+      <Row title="Trending Now"/> 
     </div>
   );
 }
